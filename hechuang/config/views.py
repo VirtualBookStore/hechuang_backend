f5479298e339c117b9643f0cee11a2946f732671
rand1925 <rand1925@outlook.com>
2021-01-09 15:34:04 +0800

    finish

--- hechuang/config/views.py
+++ hechuang/config/views.py
@@ -23,6 +23,8 @@ class SiteConfigurationView(RetrieveUpdateAPIView):
 
     recycle_rate 回收的折扣率，0-1其中0代表没有折扣，1代表白送
     """
+    object = SiteConfiguration.get_solo()
+    queryset = 
     permission_classes = (IsAdminUserOrReadOnly,)
     serializer_class = SiteConfigurationSerializer
     schema = AutoSchema(
--- hechuang/recycle/models.py
+++ hechuang/recycle/models.py
@@ -6,8 +6,3 @@ from order.models import Order
 
 class RecycleRequest(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE, primary_key=True)
-    message = models.TextField(blank=True)
-    price = models.IntegerField(default=0)
-    allowed = models.BooleanField(null=True)
-    read = models.BooleanField(default=False)
-    number = models.IntegerField(default=1)
